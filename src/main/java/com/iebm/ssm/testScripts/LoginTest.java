c2e7586f9864630c101c925e5b0978a40d6e5d18
LC <sophine_lc@163.com>
2019-03-05 17:25:12 +0800

    selenium测试代码

--- src/main/java/com/iebm/ssm/testScripts/LoginTest.java
+++ src/main/java/com/iebm/ssm/testScripts/LoginTest.java
@@ -6,9 +6,7 @@ import com.iebm.ssm.util.Constant;
 import com.iebm.ssm.util.Log;
 import com.iebm.ssm.util.OpenBrower;
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /*
  *TODO
@@ -20,11 +18,17 @@ import org.testng.annotations.Test;
 public class LoginTest {
 
 	String baseUrl = Constant.url;
-	
+
+	@DataProvider(name="loginInfo")
+	public static Object[][] loginInfo(){
+		return  new Object[][]{{"ssm","888888"},{"1001","666666"}};
+	}
+
+
 	@Test(testName = "用户登录")
 	public void testLogin() throws Exception{
 		Log.info("调用Login_Action.execute");
-		Login_Action.execute(Constant.driver, Constant.loginUsername, Constant.loginPassword);
+		Login_Action.execute(Constant.loginUsername, Constant.loginPassword);
 		Thread.sleep(3000);
 		Assert.assertTrue(Constant.driver.getPageSource().contains("退出系统"));
 
@@ -33,20 +37,36 @@ public class LoginTest {
 	@Test(testName = "管理员登录")
 	public void admintLogin() throws Exception{
 		Log.info("调用Login_Action.execute");
-		Login_Action.execute(Constant.driver, "1001", "666666");
+		Login_Action.execute("1001", "666666");
 		Thread.sleep(3000);
 		Assert.assertTrue(Constant.driver.getPageSource().contains("退出系统"));
 
 	}
+
+	@Test(dataProvider = "loginInfo")
+	public void dataDriverLogin(String loginid,String password) throws Exception{
+		Log.info("使用DataDriver测试");
+		Login_Action.execute(loginid, password);
+		Thread.sleep(3000);
+		Assert.assertTrue(Constant.driver.getPageSource().contains("退出系统"));
+	}
 	
 	@BeforeTest
-	public void beforeMethod(){
+	public void beforeTest(){
 		Constant.driver =OpenBrower.openBrowser("chrome");
 		Constant.driver.get(Constant.url);
 	}
-	
+
+
+	@AfterMethod
+	public void afterMethod() throws Exception{
+//		Login_Action.loginOutexecute();
+//		Thread.sleep(1000);
+	}
+
+
 	@AfterSuite
-	public void afterMethod(){
+	public void afterSuite(){
 //		Constant.driver.quit();
 	}
 
