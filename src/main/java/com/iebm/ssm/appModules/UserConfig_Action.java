223fd571993fadd5a26d9dd5547d3af0de66c8a6
LC <sophine_lc@163.com>
2019-03-07 11:10:04 +0800

    selenium测试代码

--- src/main/java/com/iebm/ssm/appModules/UserConfig_Action.java
+++ src/main/java/com/iebm/ssm/appModules/UserConfig_Action.java
@@ -1,6 +1,5 @@
 package com.iebm.ssm.appModules;
 
-import com.iebm.ssm.pageObjects.UserAddPage;
 import com.iebm.ssm.pageObjects.UserConfigPage;
 import com.iebm.ssm.util.Constant;
 import com.iebm.ssm.util.Table;
@@ -8,7 +7,6 @@ import com.iebm.ssm.util.TreeSelector;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 
 import java.util.Map;
 
@@ -17,12 +15,10 @@ import java.util.List;
 public class UserConfig_Action {
 
     private UserConfigPage page;
-    private UserAddPage addpage;
 
     public UserConfig_Action(){
 
         page = new UserConfigPage(Constant.driver);
-        addpage = new UserAddPage(Constant.driver);
     }
 
     public void openPage() throws Exception {
@@ -74,53 +70,53 @@ public class UserConfig_Action {
     public void addUser(Map userInfo) throws Exception {
         page.addUserBtn().click();
 
-        addpage.userAddwindow_maxsize_btn().click();
+        page.userAddwindow_maxsize_btn().click();
         if (userInfo.get("loginId") != null) {
-            addpage.loginId_input().sendKeys((String) userInfo.get("loginId"));
+            page.add_loginId_input().sendKeys((String) userInfo.get("loginId"));
         }
         if (userInfo.get("name") != null) {
-            addpage.name_input().sendKeys((String)userInfo.get("name"));
+            page.add_name_input().sendKeys((String)userInfo.get("name"));
         }
 
         if (userInfo.get("displayName") != null) {
-            addpage.displayName_input().click();
+            page.add_displayName_input().click();
             if(!((String)userInfo.get("name")).equals((String)userInfo.get("displayName"))){
-                addpage.displayName_input().click();
-                addpage.displayName_input().clear();
-                addpage.displayName_input().sendKeys((String)userInfo.get("displayName"));
+                page.add_displayName_input().click();
+                page.add_displayName_input().clear();
+                page.add_displayName_input().sendKeys((String)userInfo.get("displayName"));
             }
 
         }
         if (userInfo.get("postid") != null) {
-            Select select = new Select(addpage.postid_option());
+            Select select = new Select(page.add_postid_option());
             select.selectByVisibleText((String)userInfo.get("postid"));
         }
         if (userInfo.get("officePhoneNO") != null) {
-            addpage.officePhoneNO_input().sendKeys((String)userInfo.get("officePhoneNO"));
+            page.add_officePhoneNO_input().sendKeys((String)userInfo.get("officePhoneNO"));
         }
         if (userInfo.get("mobilePhoneNO") != null) {
-            addpage.mobilePhoneNO_input().sendKeys((String)userInfo.get("mobilePhoneNO"));
+            page.add_mobilePhoneNO_input().sendKeys((String)userInfo.get("mobilePhoneNO"));
         }
         if (userInfo.get("email") != null) {
-            addpage.email_input().sendKeys((String)userInfo.get("email"));
+            page.add_email_input().sendKeys((String)userInfo.get("email"));
         }
         if (userInfo.get("faxNumber") != null) {
-            addpage.faxNumber_input().sendKeys((String)userInfo.get("faxNumber"));
+            page.add_faxNumber_input().sendKeys((String)userInfo.get("faxNumber"));
         }
         if (userInfo.get("bindIp") != null&&userInfo.get("bindIp") !="") {
-            addpage.ipBindFlag_true().click();
-            addpage.bindIp_input().sendKeys((String)userInfo.get("bindIp"));
+            page.add_ipBindFlag_true().click();
+            page.add_bindIp_input().sendKeys((String)userInfo.get("bindIp"));
         }
         if (userInfo.get("areaName") != null&&userInfo.get("areaName") !="") {
-            addpage.areaId_img().click();
-            TreeSelector tree = new TreeSelector(addpage.areaId_div());
+            page.add_areaId_img().click();
+            TreeSelector tree = new TreeSelector(page.add_areaId_div());
             tree.expandTree();
             tree.chooseTree((String)userInfo.get("areaName"));
-            addpage.areaConfirm_btn().click();
-//            addpage.areaClose_btn().click();
+            page.add_areaConfirm_btn().click();
+//            page.areaClose_btn().click();
         }
         if (userInfo.get("institution") != null&&userInfo.get("institution") !="") {
-            addpage.institutionId_img().click();
+            page.add_institutionId_img().click();
             Thread.sleep(500);
             HospitalSelect_Action hospitalselect = new HospitalSelect_Action();
             hospitalselect.findHospital((String)userInfo.get("institution"));
@@ -128,10 +124,10 @@ public class UserConfig_Action {
         }
 
         if (userInfo.get("remark") != null) {
-            addpage.remark_input().sendKeys((String)userInfo.get("remark"));
+            page.add_remark_input().sendKeys((String)userInfo.get("remark"));
         }
 
-        addpage.userAddsave_btn().click();
+        page.add_userAddsave_btn().click();
         Thread.sleep(500);
         String s = Constant.driver.getPageSource();
 //        System.out.println(s);
@@ -140,7 +136,7 @@ public class UserConfig_Action {
         }else{
             System.out.println("用户"+userInfo.get("loginId")+"添加失败");
         }
-//        addpage.userAddMsg_btn().click();
+//        page.userAddMsg_btn().click();
 
 
 
--- src/main/java/com/iebm/ssm/pageObjects/UserConfigPage.java
+++ src/main/java/com/iebm/ssm/pageObjects/UserConfigPage.java
@@ -4,6 +4,8 @@ import com.iebm.ssm.util.ObjectMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class UserConfigPage {
 
     private WebElement element = null;
@@ -34,5 +36,110 @@ public class UserConfigPage {
         return element;
     }
 
+    public WebElement userAddwindow_maxsize_btn() throws Exception{
+        List<WebElement> elementList = driver.findElements(objectmap.getLocator("userAddwindow_maxsize_btn"));
+        return elementList.get(0);
 
+    }
+
+    public WebElement add_loginId_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_loginId_input"));
+        return element;
+    }
+
+    public WebElement add_name_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_name_input"));
+        return element;
+    }
+
+    public WebElement add_displayName_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_displayName_input"));
+        return element;
+    }
+
+    public WebElement add_postid_option() throws Exception{
+        element = driver.findElement(objectmap.getLocator("Add_postid_option"));
+        return element;
+    }
+
+    public WebElement add_officePhoneNO_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_officePhoneNO_input"));
+        return element;
+    }
+
+    public WebElement add_mobilePhoneNO_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_mobilePhoneNO_input"));
+        return element;
+    }
+
+    public WebElement add_email_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_email_input"));
+        return element;
+    }
+
+    public WebElement add_faxNumber_input() throws Exception{
+        element = driver.findElement(objectmap.getLocator("Add_faxNumber_input"));
+        return element;
+    }
+
+    public WebElement add_ipBindFlag_true() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_ipBindFlag_true"));
+        return element;
+    }
+
+    public WebElement add_ipBindFlag_false() throws Exception{
+        element = driver.findElement(objectmap.getLocator("Add_ipBindFlag_false"));
+        return element;
+    }
+
+    public WebElement add_bindIp_input() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_bindIp_input"));
+        return element;
+    }
+
+    public WebElement add_areaId_img() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_areaId_img"));
+        return element;
+    }
+
+    public WebElement add_areaClose_btn() throws Exception {
+        List<WebElement> elementList = driver.findElements(objectmap.getLocator("Add_areaClose_btn"));
+        return elementList.get(1);
+    }
+
+    public WebElement add_areaConfirm_btn() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_areaConfirm_btn"));
+        return element;
+    }
+
+
+    public WebElement add_areaId_div() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_areaTree_div"));
+        return element;
+    }
+
+    public WebElement add_institutionId_img() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_institutionId_img"));
+        return element;
+    }
+
+    public WebElement add_remark_input() throws Exception{
+        element = driver.findElement(objectmap.getLocator("Add_remark_input"));
+        return element;
+    }
+
+    public WebElement add_userAddsave_btn() throws Exception {
+        element = driver.findElement(objectmap.getLocator("Add_userAddsave_btn"));
+        return element;
+    }
+
+    public WebElement add_userAddreset_btn() throws Exception{
+        element = driver.findElement(objectmap.getLocator("Add_userAddreset_btn"));
+        return element;
+    }
+
+    public WebElement add_userAddMsg_btn() throws Exception{
+        element = driver.findElement(objectmap.getLocator("Add_userAddMsg_btn"));
+        return element;
+    }
 }
