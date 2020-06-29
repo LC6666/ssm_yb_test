4d567e5fcdb63c971333f84111c5e70dbe305eeb
LC6666 <sophine_lc@163.com>
2020-06-30 07:58:48 +0800

    123

---
+++ src/test/java/com/iebm/ssm/test/Encrypt.java
@@ -0,0 +1,144 @@
+package com.iebm.ssm.test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Properties;
+
+import javax.crypto.Cipher;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.springframework.beans.factory.FactoryBean;
+
+
+/**
+ * 
+ * @ClassName: Encrypt 
+ * @Description: 閺佺増宓佹惔鎾舵暏閹村嘲鎮曢崝鐘盒掔�碉拷
+ * @author HuZongJian
+ * @date 2013楠烇拷1閺堬拷閺冿拷娑撳宕�2:17:36 
+ *
+ */
+public class Encrypt  implements FactoryBean<Object> {
+	private static final Log logger = LogFactory.getLog(Encrypt.class);
+	private Properties properties;
+
+	public Object getObject() throws Exception {
+		return getProperties();
+	}
+
+	public Class<Properties> getObjectType() {
+		return java.util.Properties.class;
+	}
+
+	public boolean isSingleton() {
+		return true;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties inProperties) {
+		this.properties = inProperties;
+		String originalUsername = properties.getProperty("user");
+		String originalPassword = properties.getProperty("password");
+		if (originalUsername != null) {
+			String newUsername = deEncryptUsername(originalUsername);
+			properties.put("user", newUsername);
+		}
+		if (originalPassword != null) {
+			String newPassword = deEncryptPassword(originalPassword);
+			properties.put("password", newPassword);
+		}
+	}
+
+	private String deEncryptUsername(String originalUsername) {
+		return dCode(originalUsername.getBytes());
+	}
+
+	private String deEncryptPassword(String originalPassword) {
+		return dCode(originalPassword.getBytes());
+	}
+
+	public String eCode(String needEncrypt) {
+		byte result[] = null;
+		try {
+			Cipher enCipher = Cipher.getInstance("DES");
+			javax.crypto.SecretKey key = EncryptKey.loadKey();
+			enCipher.init(1, key);
+			result = enCipher.doFinal(needEncrypt.getBytes());
+			BASE64Encoder base64Encoder = new BASE64Encoder();
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			base64Encoder.encode(result, bos);
+			result = bos.toByteArray();
+		} catch (Exception e) {
+			throw new IllegalStateException(
+					"System doesn't support DES algorithm.");
+		}
+		return new String(result);
+	}
+
+	public String dCode(byte result[]) {
+		String s = null;
+		try {
+			Cipher deCipher = Cipher.getInstance("DES");
+			deCipher.init(2, EncryptKey.loadKey());
+			BASE64Decoder base64Decoder = new BASE64Decoder();
+			result = base64Decoder.decodeBuffer(new String(result));
+			byte strByte[] = deCipher.doFinal(result);
+			s = new String(strByte);
+		} catch (Exception e) {
+			throw new IllegalStateException(
+					"System doesn't support DES algorithm.");
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+//		String s = "zzqbj_ssmmid";
+//		String s = "ordos_ssm";
+//		String s = "ssm_eeds";
+//		String s = "ssm_new";
+		// String s = "+LFyPEiaZxdV+Ir/vG8gkA=="
+	     String s = "ebm83881021";
+		String s1 = "jdbc:oracle:thin:@192.168.29.144:1521:orcl";
+		//String s = "ulm_ssm";
+		Encrypt p = new Encrypt();
+		String afterE = p.eCode(s);
+		System.out.println("after s:" + afterE);
+		System.out.println("s1:" + p.eCode(s1));
+		System.out.println(1);
+		System.out.println("before:" + p.dCode("/WoSAA//pvYvv2QcudGpZQ==".getBytes()));
+		System.out.println("before:" + p.dCode("MqhP4FEa0uAF7UfSFO44Xw==".getBytes()));
+		System.out.println("before:" + p.dCode("Kbs2u6NELkMD+i6RnR+aSVshbAPcIa9Or5MUq0SHUFNNGguiRfIOeVX4iv+8byCQ".getBytes()));
+		StringBuilder sb=new StringBuilder("72pc:");
+		sb.appendCodePoint(72);
+		System.out.println(sb.toString());
+		//System.out.println('鐪�');
+	}
+	
+	@Test
+	public void test() {
+//		String s = "zzqbj_ssmmid";
+//		String s = "ordos_ssm";
+//		String s = "ssm_eeds";
+//		String s = "ssm_new";
+		// String s = "+LFyPEiaZxdV+Ir/vG8gkA=="
+//		String s = "ssm_bt_new";
+//		String s = "tc_ssm_test";
+	//	String s = "jdbc:oracle:thin:@192.168.29.171:1521:orcl";
+		String s1 = " als_ssm";
+		String s = "jdbc:oracle:thin:@192.168.29.171:1521:orcl";
+		//String s = "ulm_ssm";
+		Encrypt p = new Encrypt();
+		String afterE = p.eCode(s);
+		System.out.println("after:" + afterE);
+		System.out.println("after:" + p.eCode(s1));
+		System.out.println("before:" + p.dCode("pqODKd/CU1AF7UfSFO44Xw==".getBytes()));
+		System.out.println("before:" + p.dCode("szCx9JW4gLiIFegxTyEfbA==".getBytes()));
+		System.out.println("before:" + p.dCode("Kbs2u6NELkMD+i6RnR+aSQQSJDG9EpkVQpiEHUomAvG9BkQOdPdZGBgCtVrBwlQv".getBytes()));
+		//System.out.println('鐪�');
+	}
+	
+}
--- src/test/java/com/iebm/ssm/test/OracleUpdateTest.java
+++ src/test/java/com/iebm/ssm/test/OracleUpdateTest.java
@@ -11,41 +11,54 @@ import java.sql.SQLException;
 
 public class OracleUpdateTest {
 
+	/**
+	 * 数据库配置
+	 * @return
+	 */
 	private static String[][] getDBinfo(){
         String[][] dbinfo = {
-//                185
+        		{"jdbc:oracle:thin:@192.168.29.139:1521:orcl","yf_ssm_test01","yf_ssm_test01","139","240"},
+        		
+        		{"jdbc:oracle:thin:@192.168.29.171:1521:orcl","elht_ssm_test","elht_ssm_test","171","223"},               
+                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","nm_mzl_ssm_test","nm_mzl_ssm_test","171","223"},
+                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","nm_local_ssm_test","nm_local_ssm_test","171","185"},
+                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","wh_ssm_test","wh_ssm_test","171","223"},
+                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","wlcb_ssm_test","wlcb_ssm_test","171","185"},
+                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","wlcb_drgs_ssm_test","wlcb_drgs_ssm_test","171","240"},
+                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","tc_ssm_test","tc_ssm_test","171"},
 
-                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","als_ssm_test","als_ssm_test","240"},
-                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","byze_ssm_test","byze_ssm_test","240"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","bt_ssm_test","bt_ssm_test","223"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","cf_ssm_test","cf_ssm_test","223"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","eeds_ssm_test","eeds_ssm_test","223"},
-                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","elht_ssm_test","elht_ssm_test","171"},
-                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","hhht_ssm_test","hhht_ssm_test","240"},
-                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","hlbe_ssm_test","hlbe_ssm_test","240"},
-                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","nm_mzl_ssm_test","nm_mzl_ssm_test","171"},
-                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","nm_local_ssm_test","nm_local_ssm_test","171"},
-//                
-//                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","nm_local_ssm2_test","nm_local_ssm2_test"},
-                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","bj_ssm_test","bj_ssm_test","249"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","tl_ssm_test","tl_ssm_test","223"},
-                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","wh_ssm_test","wh_ssm_test","171"},
-                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","wlcb_ssm_test","wlcb_ssm_test","171"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","xlgl_ssm_test","xlgl_ssm_test","223"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","xam_ssm_test","xam_ssm_test","223"},
+                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","als_ssm_test","als_ssm_test","240","240"},
+                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","byze_ssm_test","byze_ssm_test","240","240"},
+                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","hhht_ssm_test","hhht_ssm_test","240","240"},
+                {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","hlbe_ssm_test","hlbe_ssm_test","240","240"},
                 {"jdbc:oracle:thin:@192.168.29.240:1521:orcl","rules_ssm_test","rules_ssm_test"},
-//                {"jdbc:oracle:thin:@192.168.29.139:1521:orcl","ssm_sy_new","ssm_sy_new"},
-                {"jdbc:oracle:thin:@192.168.29.139:1521:orcl","yf_ssm_test01","yf_ssm_test01","139"},
+                
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","bt_ssm_test","bt_ssm_test","223","185"},
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","cf_ssm_test","cf_ssm_test","223","223"},
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","eeds_ssm_test","eeds_ssm_test","223","223"},
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","tl_ssm_test","tl_ssm_test","223","223"},
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","xlgl_ssm_test","xlgl_ssm_test","223","223"},
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","xam_ssm_test","xam_ssm_test","223","185"},
+                
+                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","bj_ssm_test","bj_ssm_test","249"},
+//                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","tc_ssm_test","tc_ssm_test","249"},
+                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","sy_drgs_ssm","sy_drgs_ssm","249","249"},
+                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","yf_ssm_test02","yf_ssm_test02","223","240"}
+                
+                
+//                
+//                {"jdbc:oracle:thin:@192.168.29.171:1521:orcl","nm_local_ssm2_test","nm_local_ssm2_test"},                
+//                {"jdbc:oracle:thin:@192.168.29.139:1521:orcl","ssm_sy_new","ssm_sy_new"},                
 //                {"jdbc:oracle:thin:@192.168.29.115:1521:orcl","ssm_sy_new","ssm_sy_new"},
 //                {"jdbc:oracle:thin:@192.168.29.115:1521:orcl","wlcb_ssm_test","wlcb_ssm_test"},
 
 
-                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","tc_ssm_test","tc_ssm_test","249"},
+
 //                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","bt_ssm_test","bt_ssm_test"},宸插純鐢�
 //                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","wh_ssm_test","wh_ssm_test"},宸插純鐢�
-                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","sy_drgs_ssm","sy_drgs_ssm","249"},
+                
 //                {"jdbc:oracle:thin:@192.168.29.249:1521:orcl","yf_ssm_test01","yf_ssm_test01"},
-                {"jdbc:oracle:thin:@192.168.29.223:1521:orcl","yf_ssm_test02","yf_ssm_test02","223"}
+                
 
         };
 
@@ -54,19 +67,35 @@ public class OracleUpdateTest {
 
     public String setSQL(){
 
-        String sql ="update file_resource_pre set file_path='' , old_path='' where oid in(1375,1376)" ;
+        String sql ="INSERT INTO \"FW_SCHEDULE_IP\"(\"ID_\", \"SCHEDULEID_\", \"SERVERIP_\", \"CREATEBY_\", \"CREATETIME_\", \"LASTUPDATEBY_\", \"LASTUPDATETIME_\", \"ENABLE_\") VALUES ('1164', '1001', '192.168.29.%s', '10000', TO_TIMESTAMP('2020-06-02 14:17:20.997000', 'SYYYY-MM-DD HH24:MI:SS:FF6'), '10000', TO_TIMESTAMP('2020-06-02 14:17:20.997000', 'SYYYY-MM-DD HH24:MI:SS:FF6'), '1')";
+		
+        
         return sql;
     }
 
     public String[] setSQLs(){
         String[] sqls = {
-//                "drop table violation_source",
-//                "drop table file_resource_pre",
-//                "drop table wg_medical_file_zd"
-                };
+        		"alter table SI_MEDICAL_INFORMATION_HIS_TMP add sing_dis_set_flag varchar2(10)",
+        		"alter table SI_MEDICAL_INFORMATION_HIS_TMP add complications_flag varchar2(10)",
+        		"alter table SI_MEDICAL_INFORMATION_HIS_TMP add quit_sing_dis_flag varchar2(10)",
+        		"alter table SI_MEDICAL_INFORMATION_HIS_TMP add quit_sing_dis_dept varchar2(20)",
+        		"alter table SI_MEDICAL_INFORMATION_HIS_TMP add quit_sing_dis_cause varchar2(500)",
+        		"comment on column SI_MEDICAL_INFORMATION_HIS_TMP.sing_dis_set_flag is '普通住院单病种结算标志（1：是，其他不是）'",
+        		"comment on column SI_MEDICAL_INFORMATION_HIS_TMP.complications_flag is '0 无 1 存在合并症 2 存在并发症(手术过程中) 3 同时存在合并症、并发症(手术过程中) '",
+        		"comment on column SI_MEDICAL_INFORMATION_HIS_TMP.quit_sing_dis_flag is '退出单病种结算标志（1：是，其他不是）'",
+        		"comment on column SI_MEDICAL_INFORMATION_HIS_TMP.quit_sing_dis_dept is '申请退出单病种结算科室'",
+        		"comment on column SI_MEDICAL_INFORMATION_HIS_TMP.quit_sing_dis_cause is '申请退出单病种结算原因'"
+        		
+        		
+        		
+        };
         return sqls;
     }
     
+    /**
+     * 代入参数生成导入语句
+     * @return
+     */
     public String[] newSqls(){
 //    	String impdp_str = "impdp %s/%s directory=DUMP_DIR transform=segment_attributes:n dumpfile=rule_filrs.dmp remap_schema=bj_ssm_test:%s remap_tablespace=bj_ssm_test:%s;";
 //    	String imp_str = "imp system/abc-123@192.168.29.249:1521:orcl file=/opt/oracle/dump/rule_filrs.dmp fromuser=bj_ssm_test touser=%s full=y ignore=y;";
@@ -76,9 +105,10 @@ public class OracleUpdateTest {
     		str_arrs[i] = String.format(imp_str, getDBinfo()[i][1],getDBinfo()[i][2],getDBinfo()[i][3]);
     		System.out.println(str_arrs[i]);
     	}
-    	return null;
+    	return str_arrs;
     }
 
+//    单一sql语句执行
     public void updateSql() throws SQLException{
 
         String sql = this.setSQL();
@@ -87,7 +117,8 @@ public class OracleUpdateTest {
         for (int i = 0;i<dbinfo.length;i++){
             Connection conn = ora.getConn(dbinfo[i][0],dbinfo[i][1] , dbinfo[i][2]);
             System.out.println(dbinfo[i][1]);
-//            System.out.println(sql);
+//            String new_sql = sql.format(sql, dbinfo[i][4]);
+//            System.out.println(new_sql);
             ora.executesql(sql);
             ora.closeCon(conn);
 
@@ -96,6 +127,7 @@ public class OracleUpdateTest {
     }
 
 
+//    多sql语句执行
     public void updateSqls() throws SQLException{
 
         String[] sqls = this.setSQLs();
@@ -116,9 +148,9 @@ public class OracleUpdateTest {
 
     public static void main(String[] args) throws SQLException {
         OracleUpdateTest update = new OracleUpdateTest();
-//        update.updateSqls();
+        update.updateSqls();
 //        update.updateSql();
-        update.newSqls();
+//        update.newSqls();
 
 
     }
